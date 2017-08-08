ed494692c14091a47fd6f96d138296e3c8989998
Marcio Vieira <marciovieira@Marcios-MacBook-Pro.local>
2017-08-08 22:32:00 +0100

    no message

---
+++ Game1/Assets/source/Jump.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour {
+
+	private int jumps = 2;
+	public float strengh = 100f;
+	public Rigidbody2D character;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (jumps > 0) {
+			if (Input.GetKeyDown (KeyCode.Space)) {
+
+				character.AddForce (new Vector2 (0, strengh * Time.deltaTime), ForceMode2D.Impulse);
+				jumps--;	
+			}
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D body) {
+	
+		if (body.gameObject.CompareTag ("GROUND")) {
+			jumps = 2;
+		}
+	}
+
+}
---
+++ Game1/Assets/source/Walk.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walk : MonoBehaviour {
+
+	private float speed = 5f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		float horizontal = Input.GetAxis ("Horizontal");
+
+		transform.Translate (new Vector3(horizontal * speed * Time.deltaTime,0));
+	
+	}
+}
